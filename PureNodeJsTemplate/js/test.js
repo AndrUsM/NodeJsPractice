40a350bad25e1161b073ec2780f8b1ecb963e0f4
Andriy Karpenko <37944374+AndrUsM@users.noreply.github.com>
2019-07-19 20:44:26 +0300

    Update test.js

--- PureNodeJsTemplate/js/test.js
+++ PureNodeJsTemplate/js/test.js
@@ -1,1 +1,8 @@
-console.log(document.getElementsByTagName('dir'))
+if(window.document.body.scrollHeight > window.screen.availHeight){
+    document.querySelector('footer').style.position = 'relative'
+}
+else{
+    document.querySelector('footer').style.position = 'fixed'
+    document.querySelector('footer').style.left = 0
+    document.querySelector('footer').style.bottom = 0
+}
