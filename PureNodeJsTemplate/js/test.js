89e11b96433a969aa1c30c9ca72390709f295a9f
Andriy Karpenko <37944374+AndrUsM@users.noreply.github.com>
2019-07-19 20:50:01 +0300

    Update test.js

--- PureNodeJsTemplate/js/test.js
+++ PureNodeJsTemplate/js/test.js
@@ -1,8 +1,1 @@
-if(window.document.body.scrollHeight > window.screen.availHeight){
-    document.querySelector('footer').style.position = 'relative'
-}
-else{
-    document.querySelector('footer').style.position = 'fixed'
-    document.querySelector('footer').style.left = 0
-    document.querySelector('footer').style.bottom = 0
-}
+console.log(document.querySelector('header')
