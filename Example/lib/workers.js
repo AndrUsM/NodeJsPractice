b5e466469bcf9e768d275962b69008388dbe886f
Andriy Karpenko <37944374+AndrUsM@users.noreply.github.com>
2019-07-27 13:01:03 +0300

    Update workers.js

--- Example/lib/workers.js
+++ Example/lib/workers.js
@@ -3,6 +3,8 @@ const path = require('path')
 const fs = require('fs')
 const _log = require('./log')
 const os = require('os')
+const network_interfaces = os.networkInterfaces()
+const { exec } = require('child_process');
 const backup = {}
 
 workers.filename = `${new Date().getUTCFullYear()}.${new Date().getUTCMonth()}.${new Date().getUTCDate()}.${new Date().getUTCHours()}:${new Date().getUTCMinutes()}:${new Date().getUTCSeconds()}:${new Date().getUTCMilliseconds()}`
@@ -33,28 +35,6 @@ workers.create_log = async (handlers_data) => {
         throw new Error(`Error creating log ${filename}.log`)
     }
     return str
-}
-workers.loop = async (data) => {
-    setInterval( () => {
-        workers.create_log(data)
-    }, 1000 * 60) // 1 min
-}
-workers.back_up = async () => {
-    _log.createDir()
-    let current_path = path.join(__dirname, '..', '.hidden','.log')
-    let new_path = path.join(__dirname, '..', '.hidden','.backup', backup.folder_name)
-    let list = await _log.list()
-    const files_amaunt = list.length
-    try{
-        let current_file_counter = 0
-        list.map(file => {
-            fs.renameSync(path.join(current_path, `${file}.log`), path.join(new_path, `${file}.log`))
-            current_file_counter++
-            console.log(`${current_file_counter}/${files_amaunt}`)
-        })
-    }catch(err){
-        throw new Error(`Error moving files to ${new_path} folder`)
-    }
 }
 workers.loop = async (data) => {
     setInterval( () => {
